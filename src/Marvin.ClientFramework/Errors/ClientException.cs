a13f3be2cba4bd0f306c7e1be8b73fc9122609fe
Dennis Beuchler <dbeuchler@phoenixcontact.com>
2018-11-15 14:48:22 +0100

    Updated platform packages

--- src/Marvin.ClientFramework/Errors/ClientException.cs
+++ src/Marvin.ClientFramework/Errors/ClientException.cs
@@ -6,15 +6,20 @@ namespace Marvin.ClientFramework
     /// BaseClass for exceptions within Client code. It should add usable information to the System.Exception, 
     /// that may be used by a superior component/module/plugin to handle the exception.
     /// </summary>
-    public class ClientException : MarvinException
+    public class ClientException : Exception
     {
+        /// <summary>
+        /// Display text of this exception
+        /// </summary>
+        public string DisplayText { get; }
+
         /// <summary>
         /// Initializes a new instance of the <see cref="ClientException"/> class.
         /// </summary>
         public ClientException(string message, Exception innerException, string displayText)
-            : base(message, innerException, true, true, displayText)
+            : base(message, innerException)
         {
-            
+            DisplayText = displayText;
         }
     }
 }
