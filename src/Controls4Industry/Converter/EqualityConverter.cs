7ed82cdea1d78e9d359f083b3fdc545c6295d1af
Dennis Beuchler <dbeuchler@phoenixcontact.com>
2018-12-17 10:46:20 +0100

    Added EqualityConverter to compare two objects in binding

---
+++ src/Controls4Industry/Converter/EqualityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace C4I
+{
+    /// <summary>
+    /// Converter to compare two objects with <see cref="object.Equals(object)"/>
+    /// </summary>
+    public class EqualityConverter : IMultiValueConverter
+    {
+        /// <inheritdoc />
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) => 
+            values.Length >= 2 && values[0].Equals(values[1]);
+
+        /// <inheritdoc />
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
