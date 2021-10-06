fe066c5fdcfc7035e63214b4976de753a057a903
Dennis Beuchler <dbeuchler@phoenixcontact.com>
2021-10-06 13:46:40 +0200

    Updated core with IApplicationRuntime

--- src/Moryx.ClientFramework.Kernel/HeartOfLead.cs
+++ src/Moryx.ClientFramework.Kernel/HeartOfLead.cs
@@ -25,7 +25,7 @@ using MessageBoxImage = System.Windows.MessageBoxImage;
 namespace Moryx.ClientFramework.Kernel
 {
     /// <summary>
-    /// Main class to create ClientFramwork UI's
+    /// Main class to create ClientFramework UIs
     /// </summary>
     public class HeartOfLead : HeartOfLead<DefaultCommandLineArguments>
     {
@@ -36,15 +36,20 @@ namespace Moryx.ClientFramework.Kernel
     }
 
     /// <summary>
-    /// Main class to create ClientFramwork UI's
+    /// Main class to create ClientFramework UIs
     /// </summary>
-    public class HeartOfLead<TCommandLineArguments> : ILoggingHost
+    public class HeartOfLead<TCommandLineArguments> : IApplicationRuntime, ILoggingHost
         where TCommandLineArguments : DefaultCommandLineArguments
     {
         #region Fields and Properties
 
         string ILoggingHost.Name => "ClientKernel";
+
+        /// <inheritdoc />
         IModuleLogger ILoggingHost.Logger { get; set; }
+
+        /// <inheritdoc />
+        IContainer IApplicationRuntime.GlobalContainer => _container;
 
         /// <summary>
         /// Returns the current <see cref="AppConfig"/>
@@ -59,7 +64,7 @@ namespace Moryx.ClientFramework.Kernel
         /// <summary>
         /// Flag if the HeartOfLead is initialized
         /// </summary>
-        public bool IsInitialied { get; private set; }
+        public bool IsInitialied { get; private set; } // TODO: Rename to IsInitialized in the next major
 
         private GlobalContainer _container;
         private IKernelConfigManager _configManager;
@@ -101,7 +106,7 @@ namespace Moryx.ClientFramework.Kernel
             if (IsInitialied)
                 throw new InvalidOperationException("HeartOfLead is already initialized!");
 
-            // Initialize platfrom
+            // Initialize platform
             WpfPlatform.SetProduct();
 
             // Attach this Application to the console.
